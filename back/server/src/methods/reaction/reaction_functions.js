3dcca563c22fe3d6a71cc45b6600e3b05b3d3533
Solal <solal.depardieu@epitech.eu>
2021-03-02 22:10:24 +0100

    [MOD/ADD] finished github API and changed one of them to fork

--- back/server/src/methods/reaction/reaction_functions.js
+++ back/server/src/methods/reaction/reaction_functions.js
@@ -31,8 +31,8 @@ async function filterReaction(account, parameters, script_vars, reaction_type) {
             return discordSendMessage(account, parameters, script_vars);
         case 'facebook-post-page':
             return reactFacebookPostPage(account, parameters, script_vars)
-        case 'github-tag':
-            return githubTag(account, parameters, script_vars);
+        case 'github-fork':
+            return githubFork(account, parameters, script_vars);
         case 'trello-label':
             return trelloLabel(account, parameters, script_vars)
         default:
--- back/server/src/methods/reaction/reactions/github_tag.js
+++ back/server/src/methods/reaction/reactions/github_tag.js
@@ -5,27 +5,28 @@ const Action = require('@schemas/schemaAction')
 const Account = require('@schemas/schemaAccount')
 const axios = require('axios')
 
-function makeBody(title, content, description, account) {
+function getHeaderJson(access_token) {
     return {
-        body: {
-            "content": content,
-            "tts": false,
-            "embed": {
-                "title": title,
-                "description": description
-            }
-        },
         header: {
-            'Authorization': `token ${account.access_token}`,
-            'client-id': process.env.DISCORD_CLIENT_ID
+            'accept': "application/vnd.github.v3+json",
+            'Authorization': `token ${access_token}`,
+            'client-id': process.env.GITHUB_CLIENT_ID
         }
     }
 }
 
-async function githubTag(account, parameters, script_vars) {
-
+async function githubFork(account, parameters, script_vars) {
+    await axios.get('https://api.github.com/repos/' + parameters.path_to_owner + '/' + parameters.path_to_repo+ '/forks',
+        getHeaderJson(account.access_token)
+    ).then((response) => {
+        console.log(response)
+        return true
+    }).catch(e => 
+        console.log(e)
+    )
+    return false
 }
 
 module.exports = {
-    githubTag
+    githubFork
 }
