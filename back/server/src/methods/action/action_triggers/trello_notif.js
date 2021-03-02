8c81cf91b327da1cf9f81e90eb8e2261e2bd1d55
Solal <solal.depardieu@epitech.eu>
2021-03-02 16:52:14 +0100

    [ADD/MOD] filled and finished trello API (we really need to be able to test these)

--- back/server/src/methods/action/action_triggers/trello_notif.js
+++ back/server/src/methods/action/action_triggers/trello_notif.js
@@ -4,16 +4,41 @@ const Services = require('@schemas/schemaService')
 const Action = require('@schemas/schemaAction')
 const Account = require('@schemas/schemaAccount')
 
-function getHeader(access_token) {
+function makeBody(access_token) {
     return {
-        header: {
-            'Authorization': `token ${access_token}`,
-            'client-id': process.env.DISCORD_CLIENT_ID
+        body: {
+            'token': access_token,
+            'key': process.env.TRELLO_KEY
         }
     }
 }
 
 async function trelloNotif(account, parameters, script_vars, last_activation) {
+    await axios.get('https://api.trello.com/1/members/' + parameters.username + '/notifications', makeBody(account.access_token)
+    ).then((response) => {
+        console.log("Success : \n" + response);
+        if (script_vars.action_result) {
+            if (script_vars.action_result.nb_notif < response.lenght) {
+                script_vars.action_result = {
+                    'nb_notif' : response.lenght
+                }
+                return true
+            }
+            else {
+                script_vars.action_result = {
+                    'nb_notif' : response.lenght
+                }
+                return false
+            }
+        }
+        script_vars.action_result = {
+            'nb_notif' : response.lenght
+        }
+        return false
+    }).catch(e => {
+        console.log(e)
+    });
+    return false
 }
 
 
--- back/server/src/methods/reaction/reactions/trello_label.js
+++ back/server/src/methods/reaction/reactions/trello_label.js
@@ -5,25 +5,50 @@ const Action = require('@schemas/schemaAction')
 const Account = require('@schemas/schemaAccount')
 const axios = require('axios')
 
-function makeBody(title, content, description, account) {
+function makeBodyFirst(access_token) {
     return {
         body: {
-            "content": content,
-            "tts": false,
-            "embed": {
-                "title": title,
-                "description": description
-            }
-        },
-        header: {
-            'Authorization': `token ${account.access_token}`,
-            'client-id': process.env.DISCORD_CLIENT_ID
+            'token': access_token,
+            'key': process.env.TRELLO_KEY
+        }
+    }
+}
+
+function makeBody(access_token, name, board_id, color=pink) {
+    return {
+        body: {
+            'token': access_token,
+            'key': process.env.TRELLO_KEY,
+            'name': name,
+            'color': color,
+            'idBoard': board_id
         }
     }
 }
 
 async function trelloLabel(account, parameters, script_vars) {
+    let board_id = ""
+    await axios.get('https://api.trello.com/1/members/' + parameters.username + '/boards', makeBodyFirst(account.access_token)
+    ).then((response) => {
+        response.forEach(element => {
+            if (element.url == parameters.board_url || element.shortUrl == parameters.board_url)
+                board_id = element.id
+        });
+    }).catch(e => {
+        console.log(e)
+    });
 
+    if (board_id == "")
+        return false
+
+    await axios.post('https://api.trello.com/1/labels', makeBody(account.access_token, parameters.label_name, board_id, parameters.color)
+    ).then((response) => {
+        console.log("Success : \n" + response);
+        return true
+    }).catch(e => {
+        console.log(e)
+    });
+    return false
 }
 
 module.exports = {
