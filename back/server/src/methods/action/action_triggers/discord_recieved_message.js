c1d1bb81f217c3d208dce52df9debe1663bbe7df
Solal <solal.depardieu@epitech.eu>
2021-03-02 15:38:48 +0100

    [MOD] replace the default channel by a channel search (also changed parameters accordingly)

--- back/server/src/methods/action/action_triggers/discord_recieved_message.js
+++ back/server/src/methods/action/action_triggers/discord_recieved_message.js
@@ -4,7 +4,7 @@ const Services = require('@schemas/schemaService')
 const Action = require('@schemas/schemaAction')
 const Account = require('@schemas/schemaAccount')
 
-function getHeader(access_token) {
+function makeBody(access_token) {
     return {
         header: {
             'Authorization': `token ${access_token}`,
@@ -14,8 +14,20 @@ function getHeader(access_token) {
 }
 
 async function discordRecievedMessage(account, parameters, script_vars, last_activation) {
+    let channel_id = ""//"798160246794354688"
+    await axios.get('https://discord.com/api/users/@me/channels', makeBody(account.access_token)
+    ).then((response) => {
+        response.forEach(element => {
+            if (element.name == parameters.channel_name)
+                channel_id = element.id
+        });
+    }).catch(e => {
+        console.log(e)
+    });
 
-    let channel_id = "798160246794354688"
+    if (channel_id == "")
+        return false
+
     await axios.get('https://discord.com/api/channels/' + channel_id + '/messages', makeBody(account.access_token)
     ).then((response) => {
         console.log("Success : \n" + response);
--- back/server/src/methods/reaction/reactions/discord_send_message.js
+++ back/server/src/methods/reaction/reactions/discord_send_message.js
@@ -22,8 +22,30 @@ function makeBody(title, content, description, account) {
     }
 }
 
+function getHeader(access_token) {
+    return {
+        header: {
+            'Authorization': `token ${access_token}`,
+            'client-id': process.env.DISCORD_CLIENT_ID
+        }
+    }
+}
+
 async function discordSendMessage(account, parameters, script_vars) {
-    let channel_id = "798160246794354688"
+    let channel_id = ""//"798160246794354688"
+    await axios.get('https://discord.com/api/users/@me/channels', getHeader(account.access_token)
+    ).then((response) => {
+        response.forEach(element => {
+            if (element.name == parameters.channel_name)
+                channel_id = element.id
+        });
+    }).catch(e => {
+        console.log(e)
+    });
+
+    if (channel_id == "")
+        return false
+    
     await axios.post('https://discord.com/api/channels/' + channel_id + '/messages',
         makeBody(parameters.title, parameters.content, parameters.description, account)
     ).then((response) => {
